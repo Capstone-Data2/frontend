9f36ad46f0e1d930660543549de086f6b76cb73f
sverrepaulsen <61409275+sverrep@users.noreply.github.com>
2022-07-01 16:33:41 +0200

    Set up redux and axios

--- data2/src/App.js
+++ data2/src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import MainButton from './components/Buttons.js'
+import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux'
+import { fill } from './features/matches/matchesSlice'
 
 function App() {
+  const matches = useSelector((state) => state.matches.value)
+  const dispatch = useDispatch()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MainButton sx={{boxShadow: 2}} onClick={async () => dispatch(fill(await testAxios()))}>HI</MainButton>
+      <span>{matches[0]["match_id"]}</span>
     </div>
   );
+}
+
+async function testAxios() {
+  var response = []
+  await axios.get(`http://127.0.0.1:8000/matches/`)
+  .then(res => {
+    response = res.data.matches
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  return(response)
+
 }
 
 export default App;
